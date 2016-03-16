fd02c5cf1af9d3129c20a8d920e588e86ae19bbb
enlore <n.e.lorenson@gmail.com>
2016-03-16 14:22:06 -0500

    Serve task

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,60 @@ const task = g.task.bind(g)
 
 const paths = {}
 
-const defaultTasks = []
+const defaultTasks = [
+    "serve"
+]
 
-task("ship kakashi", () => {
-    console.log("ship kakashi")
-})
+task("default", defaultTasks)
+
+task("serve", serve)
+
+task("ship-all", ships)
+
+task("ship-kakashi", shipKakashi)
+task("ship-vash", shipVash)
+task("ship-ohmu", shipOhmu)
+task("ship-rei", shipRei)
+task("ship-catbus", shipCatbus)
+task("ship-seele", shipSeele)
+task("ship-cognition", shipCognition)
+
+const ships = [
+    "ship-kakashi",
+    "ship-vash",
+    "ship-ohmu",
+    "ship-rei",
+    "ship-catbus",
+    "ship-seele",
+    "ship-cognition"
+]
+
+const serverConfig = {
+    //root: path.join(__dirname, "src"),
+    root: "src",
+    //fallback: path.join(__dirname, "src", "index.html"),
+    //fallback: "index.html",
+    debug: true,
+    port: process.env.PORT || 3000
+}
+
+function serve () {
+    console.log("serving")
+    gp.connect.server(serverConfig)
+}
+
+function shipKakashi () {
+    console.log("woo")
+}
+
+function shipVash () {}
+
+function shipOhmu () {}
+
+function shipRei () {}
+
+function shipSeele () {}
+
+function shipCatbus () {}
+
+function shipCognition () {}
